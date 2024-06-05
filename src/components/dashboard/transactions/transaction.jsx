5148de09d5c333365f83a7428b354a724d5b1d4b
ashraf00963 <ashrf00963@gmail.com>
2024-06-05 10:37:55 +0200

    fixes

--- src/components/dashboard/transactions/transaction.jsx
+++ src/components/dashboard/transactions/transaction.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import $ from 'jquery';
-import './transactions.css';
+import './transaction.css';
 
 function Transactions() {
   const [transactions, setTransactions] = useState([]);
