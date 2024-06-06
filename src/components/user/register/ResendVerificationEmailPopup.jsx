b3093c59cd4b91b698b179626b2bc13d4f38ec49
ashraf00963 <ashrf00963@gmail.com>
2024-06-06 18:26:27 +0200

    resend email fixed

--- src/components/user/register/ResendVerificationEmailPopup.jsx
+++ src/components/user/register/ResendVerificationEmailPopup.jsx
@@ -35,8 +35,9 @@ function ResendVerificationEmailPopup({ onClose }) {
   };
 
   return (
-    <div className="popup">
-      <div className="popup-content">
+    <div className="popup__resend">
+      <div className="popup__resend-content">
+        <button className='close__resend' onClick={onClose}>X</button>
         <h2>Resend Verification Email</h2>
         <form onSubmit={handleResend}>
           <input
@@ -49,7 +50,7 @@ function ResendVerificationEmailPopup({ onClose }) {
           <button type="submit">Resend Email</button>
         </form>
         {message && <p>{message}</p>}
-        <button onClick={onClose}>Close</button>
+        
       </div>
     </div>
   );
